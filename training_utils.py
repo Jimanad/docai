25b71c1c7999ca357699d92a7f741d4847905b2b
anandan.kuppan@gmail.com <anandan.kuppan@gmail.com>
2020-05-02 19:03:35 +0530

    fixed

--- training_utils.py
+++ training_utils.py
@@ -409,9 +409,9 @@ def prepare_dataset(main_project_id,
   blobs = storage_client.list_blobs(input_bucket_name)
   rows_to_insert = []
   for blob in blobs:
-    resourse_uri = f"(gs://{input_bucket_name}/{blob.name})"
-    rows_to_insert.append(resourse_uri)
-    logger.info(f"Added into BQ : {resourse_uri}.")
+    resourse_uri = f"gs://{input_bucket_name}/{blob.name}"
+    rows_to_insert.append(u"".join(resourse_uri))
+   
   logger.info(f"Tupeles value : {rows_to_insert}.")
   errors = client.insert_rows(table, rows_to_insert)  # API request
   if errors == []:
